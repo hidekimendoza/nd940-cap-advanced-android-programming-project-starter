d736ec1e24429349e74c42bedaf01e2edd2c87be
Hideki Mendoza <hideki_mendoza@hotmail.com>
2022-01-03 20:14:38 -0800

    Add instrumetation test dependencies
    Added instrumentation test dependencies and
    LaunchFragmentTest dummy test
    
    Signed-off-by: Hideki Mendoza <hideki_mendoza@hotmail.com>

---
+++ starter/app/src/androidTest/java/com/example/android/politicalpreparedness/launch/LaunchFragmentTest.kt
@@ -0,0 +1,21 @@
+package com.example.android.politicalpreparedness.launch
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@MediumTest
+class LaunchFragmentTest{
+    @Test
+    fun dummy(){
+        assertThat(
+            1,
+            `is`(1)
+        )
+    }
+
+}
